3226dac4d6d0203fd9e0e5ec99e026b547873fc4
fedi-nabli <fedinabli@gmail.com>
2024-06-21 12:54:27 +0100

    Added helper scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+# ================================================
+# Utlity to setup necessary folders before build
+# ================================================
+
+create_build_dirs() {
+  mkdir -p bin build
+  echo "Directories 'bin' and 'build' are prepared."
+}
+
+cleanup_build_dirs() {
+  rm -rf bin build
+  echo "Directories 'bin' and 'build' have been deleted"
+}
+
+if [[ "$1" == '-d' ]]; then
+  cleanup_build_dirs
+else
+  create_build_dirs
+fi
