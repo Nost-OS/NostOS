42cf94bdf44bf2f915ee2e4f22a777b0fa8d75de
fedi-nabli <fedinabli@gmail.com>
2024-06-28 21:10:33 +0100

    Enabled the A20 line

--- src/boot.asm
+++ src/boot.asm
@@ -11,7 +11,7 @@ _start:
 times 33 db 0
 
 start:
-  jmp 0x7c0:boot
+  jmp 0:boot
 
 boot:
   ; Handling interrupts
@@ -72,6 +72,12 @@ load32:
   mov ss, ax
   mov ebp, 0x00200000
   mov esp, ebp
+
+  ; Enabling the A20 line
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+
   jmp $
 
 times 510-($ - $$) db 0
