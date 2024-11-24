0bf3ee979afa24bffdc1ba50144af3765bca83be
fedi-nabli <fedinabli@gmail.com>
2024-11-24 18:57:43 +0100

    Implemented the ability to change page tables

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,10 +1,29 @@
 #include "paging.h"
 
+#include <status.h>
 #include <memory/heap/kheap.h>
 
 extern void paging_load_directory(uint32_t* directory);
 
 static uint32_t* current_directory = 0;
+
+bool paging_is_aligned(void* addr);
+
+int paging_get_index(void* virtual_address, uint32_t* directory_inedx_out, uint32_t* table_index_out)
+{
+  int res = 0;
+  if (!paging_is_aligned(virtual_address))
+  {
+    return -EINVARG;
+    goto out;
+  }
+
+  *directory_inedx_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+  *table_index_out = ((uint32_t)virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+
+out:
+  return res;
+}
 
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags)
 {
@@ -38,4 +57,32 @@ void paging_switch(uint32_t* directory)
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk)
 {
   return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void* addr)
+{
+  return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
+}
+
+int paging_set(uint32_t* directory, void* virt_addr, uint32_t value)
+{
+  if (!paging_is_aligned(virt_addr))
+  {
+    return -EINVARG;
+  }
+
+  uint32_t directory_index = 0;
+  uint32_t table_index = 0;
+
+  int res = paging_get_index(virt_addr, &directory_index, &table_index);
+  if (res < 0)
+  {
+    return res;
+  }
+
+  uint32_t entry = directory[directory_index];
+  uint32_t* table = (uint32_t*)(entry & 0xffff0000);
+  table[table_index] = value;
+
+  return 0;
 }
--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -4,6 +4,7 @@
 #include <types.h>
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define PAGING_CACHE_DSIABLED   0b00010000
 #define PAGING_WRITE_THROUGH    0b00001000
@@ -24,5 +25,8 @@ void enable_paging();
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags);
 void paging_switch(uint32_t* directory);
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk);
+
+bool paging_is_aligned(void* addr);
+int paging_set(uint32_t* directory, void* virt_addr, uint32_t value);
 
 #endif
